1c4ec33492f0065e598dc76b17434192f7a4928a
albyyasmeens <156489829+albyyasmeens@users.noreply.github.com>
2024-01-16 22:24:20 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, Professor Cram!");
+        Console.WriteLine("It's nice to meet you");
+        Console.WriteLine("My name is Alby Sumague");
+        Console.WriteLine("I want to learn how to code.");
+    }
+}
